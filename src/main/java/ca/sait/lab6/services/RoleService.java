150ff19e56414522c622238b4e1ba740a98aab48
shiana-khehra <97817879+shiana-khehra@users.noreply.github.com>
2022-03-11 01:24:15 -0700

    Added the business layer
    •	UserService
    •	RoleService

---
+++ src/main/java/ca/sait/lab6/services/RoleService.java
@@ -0,0 +1,17 @@
+package ca.sait.lab6.services;
+
+import ca.sait.lab6.dataaccess.RoleDB;
+import ca.sait.lab6.models.Role;
+import ca.sait.lab6.models.User;
+import java.util.List;
+
+public class RoleService {
+    
+    private RoleDB roleDB = new RoleDB();
+    
+    public List<Role> getAll() throws Exception {
+        List<Role> roles = this.roleDB.getAll();
+        return roles;
+    }
+   
+}
